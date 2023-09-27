f9ec57c364831baf37fdb5eb5a72d71aaad63f23
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2023-09-26 21:23:11 -0600

    Added initial md and structure

---
+++ movies-app/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <section className="top-bar">
+        <input type="text" name="search" id="search" placeholder="Start typing!"/>
+      </section>
+      <section className="main">
+        <h1>Suggestions</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
