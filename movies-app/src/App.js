6f52a88f750a94262fbcffd569ba30f38ad7a80c
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2023-09-30 14:08:40 -0600

    Added ladpage movie gallery

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { discoverMovies } from "./api/movies";
-
+import { getImage } from "./api/images";
 function App() {
   const [movies, setMovies] = useState([]);
 
@@ -11,19 +11,45 @@ function App() {
     })
   }, []);
 
-  const renderedMovies = movies.map(movie => <MovieTile image={movie.})
+  const renderedMovies = movies.map(movie => <Card key={movie.id}><MovieTile movie={movie}/></Card>)
 
   return (
-    <div className="App">
-      {
-        console.log(movies)
-      }
+    <div className="bg-gray-800 font-jakarta h-screen flex flex-col justify-between text-zinc-200 overflow-y-scroll p-2">
+      <section className="h-10 font-bold">
+        Movies App
+      </section>
+      <section className="flex-1 flex flex-col">
+        <h1 className="mt-2 mb-6">Suggestions</h1>
+        <MovieGallery movies={renderedMovies}/>
+      </section>
     </div>
   );
 }
 
-function MovieTile({image}){
-  return <img src={image} alt="" />
+function MovieGallery({movies}){
+  return <div className="grid grid-cols-7 gap-5">
+    {movies}
+  </div> 
+}
+
+function Card({children}){
+  return <div className="bg-red-700 p-2 rounded-lg hover:bg-red-800 cursor-pointer">
+    {children}
+  </div>
+}
+
+function MovieTile({movie}){
+  const {poster_path, title, release_date, vote_average} = movie;
+  return <div className="flex flex-col">
+    <section className="rounded-lg overflow-hidden">
+      <img src={getImage(poster_path)} alt=""/>
+    </section>
+    <section className="flex flex-col font-medium flex space-y-1 mt-2 px-1">
+      <span className="title font-semibold">{title}</span>
+      <span className="release-date text-sm">{release_date}</span>
+      <span className="vote text-xs">{vote_average}</span>
+    </section>
+  </div>
 }
 
 export default App;
---
+++ movies-app/src/api/images.js
@@ -0,0 +1,5 @@
+function getImage(posterPath){
+    return `https://image.tmdb.org/t/p/original${posterPath}`;
+}
+
+export {getImage}
