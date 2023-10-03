b8d40d6d222a7c980d702097c3c92ff017b360b6
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2023-10-03 15:29:32 -0600

    Added favorites behavior

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { discoverMovies } from "./api/movies";
 import { getImage } from "./api/images";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
-import { IconContext } from "react-icons";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
   useEffect(() => {
     discoverMovies().then((response) => {
@@ -13,9 +13,17 @@ function App() {
     });
   }, []);
 
+  const handleLiked = (movie) => {
+    const isFavorite = favorites.includes(movie);
+    let updatedFavorites = [...favorites];
+    if(isFavorite) updatedFavorites = updatedFavorites.filter(fav => fav.id !== movie.id);
+    else updatedFavorites.push(movie);
+    setFavorites(updatedFavorites);
+  }
+
   const renderedMovies = movies.map((movie) => (
     <Card key={movie.id}>
-      <MovieTile movie={movie} />
+      <MovieTile movie={movie} onLiked={handleLiked}/>
     </Card>
   ));
 
@@ -24,15 +32,14 @@ function App() {
       <section className="h-10 font-bold">Movies App</section>
       <section className="flex-1 flex flex-col">
         <h1 className="mt-2 mb-6">Suggestions</h1>
-        <MovieGallery movies={renderedMovies} />
+        <div className="grid grid-cols-6 gap-5">
+          {renderedMovies}
+        </div>;
       </section>
     </div>
   );
 }
 
-function MovieGallery({ movies }) {
-  return <div className="grid grid-cols-4 gap-5">{movies}</div>;
-}
 
 function Card({ children }) {
   return (
@@ -42,13 +49,14 @@ function Card({ children }) {
   );
 }
 
-function MovieTile({ movie }) {
+function MovieTile({ movie, onLiked }) {
   const [isFavorite, setIsFavorite] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
 
   const handleFavoriteClick = (e) => {
     e.stopPropagation();
     setIsFavorite((prev) => !prev);
+    onLiked(movie);
   };
 
   const { poster_path, title, release_date, vote_average } = movie;
