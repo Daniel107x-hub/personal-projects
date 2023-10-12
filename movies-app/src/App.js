1af37f2006eb8c93979b703aa52714730bcfa684
daniel107x <DMejiaV310796@outlook.com>
2023-10-11 21:15:04 -0600

    Fixed card vertical size

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 import Card from "./components/ui/Card";
-import MovieTile from './components/movies/MovieTile';
+import MovieTile from "./components/movies/MovieTile";
 import Sidebar from "./components/ui/Sidebar";
-import { GiHamburgerMenu } from 'react-icons/gi';
+import { GiHamburgerMenu } from "react-icons/gi";
 import { discoverMovies, getMovieById } from "./api/movies";
-import loading from './loading.gif';
+import loading from "./loading.gif";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -21,12 +21,12 @@ function App() {
 
   useEffect(() => {
     const fetchMovies = async () => {
-      if(favoriteIds.length > 0){
+      if (favoriteIds.length > 0) {
         setIsLoading(true);
         try {
-          const requests = favoriteIds.map(id => getMovieById(id));
+          const requests = favoriteIds.map((id) => getMovieById(id));
           const responses = await Promise.all(requests);
-          const data = responses.map(response => response.data);
+          const data = responses.map((response) => response.data);
           setFavorites(data);
         } catch (error) {
           console.error(error);
@@ -34,47 +34,51 @@ function App() {
           // setIsLoading(false);
         }
       }
-    }
+    };
     fetchMovies();
-  }, [favoriteIds])
+  }, [favoriteIds]);
 
   const handleLiked = (id) => {
     const isFavorite = favoriteIds.includes(id);
     let updatedFavorites = [...favoriteIds];
-    if(isFavorite) updatedFavorites = updatedFavorites.filter(favId => favId !== id);
+    if (isFavorite)
+      updatedFavorites = updatedFavorites.filter((favId) => favId !== id);
     else updatedFavorites.push(id);
     setFavoriteIds(updatedFavorites);
-  }
+  };
 
   const handleSidebar = () => {
-    setShowSidebar(prev => !prev);
-  }
+    setShowSidebar((prev) => !prev);
+  };
 
   return (
-    <div className={`bg-gray-800 font-jakarta flex flex-col justify-between text-zinc-200 p-2 h-screen ${showSidebar ? 'overflow-hidden' : 'overflow-y-scroll'}`}>
+    <div
+      className={`bg-gray-800 font-jakarta flex flex-col justify-between text-zinc-200 p-2 h-screen ${
+        showSidebar ? "overflow-hidden" : "overflow-y-scroll"
+      }`}
+    >
       <section className="h-10 font-bold">Movies App</section>
       <section className="flex-1 flex flex-col relative items-center">
         <h1 className="mt-2 mb-6 text-xl">Suggestions</h1>
-        <span 
-          className={`bg-zinc-100 rounded-md text-red-500 p-2 hover:bg-zinc-200 cursor-pointer transition-all fixed z-10 ${showSidebar ? 'hidden' : 'right-[2%]'} `}
-          onClick={handleSidebar}>
-          <GiHamburgerMenu/>
+        <span
+          className={`bg-zinc-100 rounded-md text-red-500 p-2 hover:bg-zinc-200 cursor-pointer transition-all fixed z-10 ${
+            showSidebar ? "hidden" : "right-[2%]"
+          } `}
+          onClick={handleSidebar}
+        >
+          <GiHamburgerMenu />
         </span>
         <Gallery>
-          {
-            movies
-            .map((movie) => getMovieCard(movie, handleLiked, favoriteIds.includes(movie.id)))
-          }
+          {movies.map((movie) =>
+            getMovieCard(movie, handleLiked, favoriteIds.includes(movie.id))
+          )}
         </Gallery>
         <Sidebar isVisible={showSidebar} onClose={handleSidebar}>
           <h1 className="font-bold">Your favorites</h1>
           <Gallery>
-            {
-              favoriteIds.length === 0 && <h1>You have no favorites</h1>
-            }
-            {
-              favoriteIds.length > 0 && favorites.map((movie) => getMovieCard(movie, handleLiked, true))
-            }
+            {favoriteIds.length === 0 && <h1>You have no favorites</h1>}
+            {favoriteIds.length > 0 &&
+              favorites.map((movie) => getMovieCard(movie, handleLiked, true))}
           </Gallery>
         </Sidebar>
       </section>
@@ -82,18 +86,20 @@ function App() {
   );
 }
 
-function Gallery({children}){
-  return <section className="gallery flex items-center justify-center content-center">
-    {
-      children
-    }
-  </section>
+function Gallery({ children }) {
+  return (
+    <section className="gallery flex items-center justify-center content-center">
+      {children}
+    </section>
+  );
 }
 
 const getMovieCard = (movie, onLiked, isFavorite) => {
-  return <Card key={movie.id}>
-  <MovieTile movie={movie} onLiked={onLiked} isFav={isFavorite}/>
-</Card>
-}
+  return (
+    <Card key={movie.id}>
+      <MovieTile movie={movie} onLiked={onLiked} isFav={isFavorite} />
+    </Card>
+  );
+};
 
 export default App;
--- movies-app/src/components/ui/Sidebar.js
+++ movies-app/src/components/ui/Sidebar.js
@@ -1,17 +1,31 @@
-import React from 'react'
-import {AiOutlineClose} from 'react-icons/ai'
-import './Sidebar.css'
+import React from "react";
+import { AiOutlineClose } from "react-icons/ai";
+import "./Sidebar.css";
 
-function Sidebar({children, isVisible, onClose}) {
-    return (
-        <>
-            <div className={`rounded-l-xl bg-gray-700 w-[30%] h-screen top-0 z-10 fixed transition-all overflow-y-auto flex flex-col items-center text-zinc-200 p-2 ${isVisible ? 'right-0' : '-right-1/3'}`}>
-                <span className='absolute top-3 right-3 text-xl hover:text-red-400 cursor-pointer' onClick={onClose}><AiOutlineClose/></span>
-                {children}
-            </div>
-            <div className={`overlay fixed top-0 left-0 w-screen h-screen ${isVisible ? 'block' : 'hidden'}`} onClick={onClose}></div>
-        </>
-    )
+function Sidebar({ children, isVisible, onClose }) {
+  return (
+    <>
+      <div
+        className={`rounded-l-xl text-center bg-gray-700 w-[30%] h-screen top-0 z-10 fixed transition-all overflow-y-auto flex flex-col items-center text-zinc-200 p-2 ${
+          isVisible ? "right-0" : "-right-1/3"
+        }`}
+      >
+        <span
+          className="absolute top-3 right-3 text-xl hover:text-red-400 cursor-pointer"
+          onClick={onClose}
+        >
+          <AiOutlineClose />
+        </span>
+        {children}
+      </div>
+      <div
+        className={`overlay fixed top-0 left-0 w-screen h-screen ${
+          isVisible ? "block" : "hidden"
+        }`}
+        onClick={onClose}
+      ></div>
+    </>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
