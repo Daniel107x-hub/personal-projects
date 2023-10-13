82d66d95d3ffc0d54f5595a380bd2a7b9e1bda9c
Daniel Mejia <Daniel.Mejia2@Honeywell.com>
2023-10-13 17:28:52 -0600

    Added styling to movie page

--- movies-app/src/pages/Movie.js
+++ movies-app/src/pages/Movie.js
@@ -2,31 +2,29 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 import MovieTile from "../components/movies/MovieTile";
 import { getScoreStars } from "../utils/movie";
+import Card from "../components/ui/Card";
 
 function Movie() {
   const { movie } = useLoaderData();
   const {id, title, poster_path, vote_average, runtime, genres, overview} = movie;
   console.log(movie);
   return (
-    <>
-      <h1 className="mt-2 mb-6 text-xl">{movie.title}</h1>
-      <section className="flex flex-row">
-        <div className="data">
-          <h1>{title}</h1>
-          <span className="flex flex-row">{getScoreStars(vote_average)}</span>
-          <h2>{runtime}</h2>
-          <div className="genres flex flex-row space-x-2">
-            {
-              genres.map(genre => <h3>{genre.name}</h3>)
-            }
-          </div>
-          <p>{overview}</p>
+    <div className="flex flex-row justify-evenly">
+      <section className="data">
+        <h1 className="text-5xl">{title}</h1>
+        <span className="flex flex-row my-4 text-3xl">{getScoreStars(vote_average)}</span>
+        <h2>{runtime} minutes</h2>
+        <div className="genres flex flex-row space-x-2 my-2">
+          {
+            genres.map(genre => <Card>{genre.name}</Card>)
+          }
         </div>
-        <div className="w-1/4">
-          <MovieTile id={id} poster={poster_path} />
-        </div>
+        <p className="text-lg">{overview}</p>
       </section>
-    </>
+      <section className="w-1/4">
+        <MovieTile id={id} poster={poster_path} />
+      </section>
+    </div>
   );
 }
 
